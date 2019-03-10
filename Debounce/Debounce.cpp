0ce6401d86a0a5c12817967148114d1fdbad3815
Tam Nguyen <thanhtamck15@gmail.com>
2019-03-10 13:35:09 +0700

    Add project files.

---
+++ Debounce/Debounce.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		Debounce.cpp
+ Created:	3/10/2019 1:34:58 PM
+ Author:	thanh
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "Debounce.h"
+
+
---
+++ Debounce/Debounce.h
@@ -0,0 +1,19 @@
+/*
+ Name:		Debounce.h
+ Created:	3/10/2019 1:34:58 PM
+ Author:	thanh
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _Debounce_h
+#define _Debounce_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
